b9245fd4905034f6d671587eeb0762e67ded5fa2
Kevin Karma <kevinkarma55@gmail.com>
2019-10-05 19:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateDriverWithKeyAndValue(driver,key,value) {
   const newDriver = Object.assign({},driver,{[key]:value});
   return newDriver;
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
